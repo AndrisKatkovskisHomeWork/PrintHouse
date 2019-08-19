558ea1823b87df6010d276ef8b377a68b3567569
andris <crocy@inbox.lv>
2019-08-19 14:32:40 +0300

    Size (you can create/delete size)

---
+++ src/main/java/com/print/house/size/SizeRepository.java
@@ -0,0 +1,7 @@
+package com.print.house.size;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface SizeRepository extends PagingAndSortingRepository<Size, Integer> {
+
+}
